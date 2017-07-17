88743c510b7044823ecc0f83a46475ec68d03eab
shital patil <shitapatil@ptc.com>
2017-07-17 14:59:09 +0530

    Update build.sh

--- opendj-server-legacy/build.sh
+++ opendj-server-legacy/build.sh
@@ -5,6 +5,6 @@ LOWERCASENAME="windchillds"
 DOCHOMEPAGE="https://support.ptc.com/appserver/cs/portal/"
 DOCWIKIURL="http://support.ptc.com/cs/help/windchill_hc/wc110_hc/"
 DOCGUIDEURL="http://support.ptc.com/view?im_dbkey=165655"
-VERSIONQUALIFIER="Build_001"
+VERSIONQUALIFIER="Build_002"
 mvn clean
 mvn -Dproduct.name="$NAME" -Dproject.name="$FULLNAME" -DlowerCaseProductName="$LOWERCASENAME" -DdocHomepageUrl="$DOCHOMEPAGE" -DdocWikiUrl="$DOCWIKIURL" -DdocGuideRefUrl="$DOCGUIDEURL" -DdocGuideAdminUrl="$DOCGUIDEURL" -DparsedVersion.qualifier="$VERSIONQUALIFIER" -X install
