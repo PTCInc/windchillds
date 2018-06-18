58a0117d84a9cd49632b8eff3162e57fe21ae04b
rarathore <rarathore@ptc.com>
2018-06-18 12:50:04 +0530

    Update build.sh

--- opendj-server-legacy/build.sh
+++ opendj-server-legacy/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 NAME="WindchillDS"
-FULLNAME="PTC Windchill Directory Server"
+FULLNAME="Windchill Directory Server"
 LOWERCASENAME="windchillds"
 DOCHOMEPAGE="https://support.ptc.com/appserver/cs/portal/"
 DOCWIKIURL="http://support.ptc.com/cs/help/windchill_hc/wc110_hc/"
 DOCGUIDEURL="http://support.ptc.com/view?im_dbkey=165655"
-VERSIONQUALIFIER="Build_004"
+VERSIONQUALIFIER="Build_005"
 JACKSONVERSION="2.9.5"
 
 mvn -Djackson.version="$JACKSONVERSION" -Dproduct.name="$NAME" -Dproject.name="$FULLNAME" -DlowerCaseProductName="$LOWERCASENAME" -DdocHomepageUrl="$DOCHOMEPAGE" -DdocWikiUrl="$DOCWIKIURL" -DdocGuideRefUrl="$DOCGUIDEURL" -DdocGuideAdminUrl="$DOCGUIDEURL" -DparsedVersion.qualifier="$VERSIONQUALIFIER" -X install
