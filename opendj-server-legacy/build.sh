e77f430a8dbdacee54b666ecee47b834830e21e7
rarathore <rarathore@ptc.com>
2018-10-23 11:34:22 +0530

    7828105
    7828105: Fix issue related with ds replication due to java update.

--- opendj-server-legacy/build.sh
+++ opendj-server-legacy/build.sh
@@ -5,7 +5,7 @@ LOWERCASENAME="windchillds"
 DOCHOMEPAGE="https://support.ptc.com/appserver/cs/portal/"
 DOCWIKIURL="http://support.ptc.com/cs/help/windchill_hc/wc110_hc/"
 DOCGUIDEURL="http://support.ptc.com/view?im_dbkey=165655"
-VERSIONQUALIFIER="Build_007"
+VERSIONQUALIFIER="Build_008"
 JACKSONVERSION="2.9.5"
 
 mvn -Djackson.version="$JACKSONVERSION" -Dproduct.name="$NAME" -Dproject.name="$FULLNAME" -DlowerCaseProductName="$LOWERCASENAME" -DdocHomepageUrl="$DOCHOMEPAGE" -DdocWikiUrl="$DOCWIKIURL" -DdocGuideRefUrl="$DOCGUIDEURL" -DdocGuideAdminUrl="$DOCGUIDEURL" -DparsedVersion.qualifier="$VERSIONQUALIFIER" -X install
