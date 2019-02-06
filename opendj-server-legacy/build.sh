5291af9faab1c06b0a581999b027d49d463e8e0a
rarathore <rarathore@ptc.com>
2019-02-06 17:08:32 +0530

    Update build number to update WindchillDS for SPR 8057447

--- opendj-server-legacy/build.sh
+++ opendj-server-legacy/build.sh
@@ -5,7 +5,7 @@ LOWERCASENAME="windchillds"
 DOCHOMEPAGE="https://support.ptc.com/appserver/cs/portal/"
 DOCWIKIURL="http://support.ptc.com/cs/help/windchill_hc/wc110_hc/"
 DOCGUIDEURL="http://support.ptc.com/view?im_dbkey=165655"
-VERSIONQUALIFIER="Build_008"
+VERSIONQUALIFIER="Build_009"
 JACKSONVERSION="2.9.5"
 
 mvn -Djackson.version="$JACKSONVERSION" -Dproduct.name="$NAME" -Dproject.name="$FULLNAME" -DlowerCaseProductName="$LOWERCASENAME" -DdocHomepageUrl="$DOCHOMEPAGE" -DdocWikiUrl="$DOCWIKIURL" -DdocGuideRefUrl="$DOCGUIDEURL" -DdocGuideAdminUrl="$DOCGUIDEURL" -DparsedVersion.qualifier="$VERSIONQUALIFIER" -X install
